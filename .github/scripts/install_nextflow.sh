8ac10422127a059a89dbfa4413bbff5aed294f37
Dan Fornika <dfornika@gmail.com>
2024-02-08 15:22:22 -0800

    Install nextflow to /usr/local/bin

--- .github/scripts/install_nextflow.sh
+++ .github/scripts/install_nextflow.sh
@@ -6,8 +6,4 @@ echo Install Nextflow .. >> artifacts/test_artifact.log
 
 wget -qO- https://get.nextflow.io | bash
 
-mkdir -p /opt/nextflow/bin
-
-mv nextflow /opt/nextflow/bin
-
-echo "export PATH=/opt/nextflow/bin:$PATH" >> ~/.bashrc
+sudo mv nextflow /usr/local/bin/
