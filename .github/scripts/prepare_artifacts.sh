69ea1039c1c5d4a516fac5f71783395be020fbbd
Dan Fornika <dfornika@gmail.com>
2024-02-08 15:58:22 -0800

    Always prepare artifacts

--- .github/scripts/prepare_artifacts.sh
+++ .github/scripts/prepare_artifacts.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-set -eo pipefail
-
 artifacts_dir="artifacts-nextflow-${NXF_VER}"
 
 echo "Prepare artifacts .." >> ${artifacts_dir}/test.log
 
-mkdir -p artifacts/fastq
+mkdir -p ${artifacts_dir}/fastq
 
 mv .github/data/fastq/*.fastq.gz ${artifacts_dir}/fastq
-
